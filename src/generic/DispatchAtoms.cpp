0859f9bf6ac941d5e5b88eb8c6527eff1693ea84
Stephen <stephenthomas2018@gmail.com>
2018-12-11 03:12:56 +0000

    Fixing stride bug and avoiding the use of chunker

--- src/generic/DispatchAtoms.cpp
+++ src/generic/DispatchAtoms.cpp
@@ -32,7 +32,8 @@
 #include "core/ActionSet.h"
 #include "retrieve.h"
 #include "dataspaces_writer.h"
-#include "plumed_chunker.h"
+//#include "plumed_chunker.h"
+#include "chunk.h"
 #include "common.h"
 #include <vector>
 #include <tuple>
@@ -47,7 +48,7 @@
 #endif
 
 typedef std::chrono::high_resolution_clock::time_point TimeVar;
-#define duration(a) std::chrono::duration_cast<std::chrono::milliseconds>(a).count()
+typedef std::chrono::duration<double, std::milli> DurationMilli;
 #define timeNow() std::chrono::high_resolution_clock::now()
 
 using namespace std;
@@ -73,19 +74,24 @@ class DispatchAtoms:
   double lenunit;
   Retrieve* retrieve_ptr;
   DataSpacesWriter* dataspaces_writer_ptr;
-  PlumedChunker* chunker_ptr;
+  //PlumedChunker* chunker_ptr;
   int dispatch_method = 0; // 1: a4md, 2:python 
 #if defined(__PLUMED_DOES_LICHENS_DISPATCH)
   int temp=0;
 #endif
   int world_size;
   int world_rank;
-  double elapsed_time = 0.0;
-  double elapsed_time_data = 0.0;
+  double total_simulation_time_ms = 0.0;
+  double simulation_time_ms = 0.0;
+  double total_plumed_time_ms = 0.0;
+  DurationMilli plumed_time_ms;
+  //int stride = 0;
   string target;
   string python_function;
   int total_steps = 0;
-
+  int nstride = 0;
+  TimeVar t_start;
+  unsigned long int current_chunk_id = 0;
 public:
   explicit DispatchAtoms(const ActionOptions&);
   ~DispatchAtoms();
@@ -103,8 +109,8 @@ void DispatchAtoms::registerKeywords( Keywords& keys ) {
   ActionAtomistic::registerKeywords( keys );
   keys.add("compulsory","STRIDE","1","the frequency with which the atoms should be output");
   keys.add("atoms", "ATOMS", "the atom indices whose positions you would like to print out");
-  keys.add("compulsory","TOTAL_STEPS","20000","the total number of time steps being simulated");
-  keys.add("compulsory", "TARGET", "target on which to output coordinates; Options are \"a4md\" or a python module (provide the name of the .py file including the .py extension. for e.g. md_analysis.py)");
+  keys.add("compulsory","TOTAL_STEPS","1","the total number of time steps being simulated");
+  keys.add("compulsory", "TARGET","a4md","target on which to output coordinates; Options are \"a4md\" or a python module (provide the name of the .py file including the .py extension. for e.g. md_analysis.py)");
   keys.add("compulsory", "PYTHON_FUNCTION", "NONE", "name of the python module to load and execute the analysis code. Applicable only if TARGET is a .py file");
   keys.add("compulsory", "UNITS","PLUMED","the units in which to print out the coordinates. PLUMED means internal PLUMED units");
 }
@@ -120,12 +126,17 @@ DispatchAtoms::DispatchAtoms(const ActionOptions&ao):
   // Get the rank of the process
   MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
 
-    vector<AtomNumber> atoms;
+  vector<AtomNumber> atoms;
 
   parse("TOTAL_STEPS",total_steps);
-
   parse("TARGET",target);
+  //parse("STRIDE",nst);
   parse("PYTHON_FUNCTION",python_function);
+  log.printf("TOTAL_STEPS: %i\n",total_steps);
+  nstride = getStride();
+  log.printf("STRIDE: %i\n",nstride);
+  log.printf("TARGET: %s\n",target.c_str());
+  log.printf("PYTHON_FUNCTION: %s\n",python_function.c_str());
   if(target.length()==0) error("name out output target was not specified");
 
   
@@ -141,9 +152,10 @@ DispatchAtoms::DispatchAtoms(const ActionOptions&ao):
   }
   else if (target == "a4md")
   {
-    chunker_ptr = new PlumedChunker();
+    //chunker_ptr = new PlumedChunker();
     char* temp_var_name = "test_var";
-    dataspaces_writer_ptr = new DataSpacesWriter(temp_var_name);
+    unsigned long int total_chunks = total_steps/nstride + 1;// +1 for the first call before starting simulation
+    dataspaces_writer_ptr = new DataSpacesWriter(temp_var_name, total_chunks);
     dispatch_method = 1;
   }
   else
@@ -156,18 +168,32 @@ DispatchAtoms::DispatchAtoms(const ActionOptions&ao):
 
   checkRead();
 
-  log.printf("  dispatching the following atoms in %s :", unitname.c_str() );
-  for(unsigned i=0; i<atoms.size(); ++i) log.printf(" %d",atoms[i].serial() );
-  log.printf("\n");
+  //log.printf("  dispatching the following atoms in %s :", unitname.c_str() );
+  //for(unsigned i=0; i<atoms.size(); ++i) log.printf(" %d",atoms[i].serial() );
+  //log.printf("\n");
   requestAtoms(atoms);
 }
 
 void DispatchAtoms::update() {
     //printf("Number of atom positions being dispatched: %d rank %d/%d\n",getNumberOfAtoms(),world_rank,world_size);
-  if (world_rank ==0)
+  if (world_rank == 0)
   {
-    TimeVar t1=timeNow();
-    int step = 0;
+    auto step = getStep(); 
+    if (step > 0)
+    {
+        DurationMilli round_about_time_ms = timeNow() - t_start;
+        auto prev_simulation_time_ms = round_about_time_ms.count() - plumed_time_ms.count();
+        total_simulation_time_ms += prev_simulation_time_ms;
+        total_plumed_time_ms += plumed_time_ms.count();
+    }
+    if (step == total_steps)
+    {
+        log.printf("total_dispatch_action_time_ms : %f\n",total_plumed_time_ms);
+        log.printf("total_simulation_time_ms : %f\n",total_simulation_time_ms);
+        log.printf("total_time_steps : %d\n",total_steps);
+    }
+    t_start = timeNow();
+
     if (dispatch_method == 1)//a4md
     {
       const Tensor & t(getPbc().getBox());
@@ -181,12 +207,11 @@ void DispatchAtoms::update() {
 
       int atom_count = getNumberOfAtoms();
 
-      step=getStep(); 
       //printf("In DISPATCH step: %d\n",step);
       bool wf3 = true;
-      if (wf3)
+      if (dispatch_method==1)
         {
-          printf("Preparing data to send to dataspace, step %i\n",step);
+          //printf("Preparing data to send to dataspace, step %i\n",step);
           //std::vector<std::vector<double> > positions;
           std::vector<double> x_positions;
           std::vector<double> y_positions;
@@ -202,21 +227,24 @@ void DispatchAtoms::update() {
             //positions.push_back(pos_tuple);
             types.push_back(0);
           }
-
-          chunker_ptr->append(step,
-                         types,
-                         x_positions,
-                         y_positions,
-                         z_positions,
-                         lx,ly,lz,
-                         xy,xz,yz);
-
-          int num_chunks = 1;
-          auto chunks = chunker_ptr->get_chunks(num_chunks);
+          PLMDChunk plmd_chunk(current_chunk_id++,
+                               step,
+                               types,
+                               x_positions,
+                               y_positions,
+                               z_positions,
+                               lx,
+                               ly,
+                               lz,
+                               xy,
+                               xz,
+                               yz);
+          Chunk* chunk = &plmd_chunk; 
+          std::vector<Chunk*> chunks = {chunk};
           dataspaces_writer_ptr->write_chunks(chunks);
 
         }
-        else if (dispatch_method == 1)
+        else if (dispatch_method == 2)
         {
           std::vector<double> x_positions;
           std::vector<double> y_positions;
@@ -232,7 +260,6 @@ void DispatchAtoms::update() {
             //positions.push_back(pos_tuple);
             types.push_back(0);
           }
-          elapsed_time_data += duration(timeNow()-t1);
           retrieve_ptr->analyze_frame(types,
                                       x_positions,
                                       y_positions,
@@ -248,18 +275,11 @@ void DispatchAtoms::update() {
         else
           log.printf(" ERROR: Unknown TARGET specified in DispatchAtoms Action.\n");
     }
-    elapsed_time += duration(timeNow()-t1);
-    if (step >= total_steps)
-    {
-      printf("DISPATCH_UPDATE_TIME_rank_%d : %f\n",world_rank,elapsed_time);
-      printf("DISPATCH_DATA_TIME_rank_%d : %f\n",world_rank,elapsed_time_data);
-    }
+    plumed_time_ms = timeNow()-t_start;
   }
 }
 
 DispatchAtoms::~DispatchAtoms() {
-  chunker_ptr = NULL;
-  delete chunker_ptr;
   dataspaces_writer_ptr = NULL;
   delete dataspaces_writer_ptr;
   retrieve_ptr = NULL;
